fd46764af1d1045484671b65d55873beb02ac89c
Leonid M <eleoneks@gmail.com>
2020-11-27 19:53:26 +0200

    Add homework for lesson 1

---
+++ neotology/home_work_1.py
@@ -0,0 +1,12 @@
+#REPL.IT link
+#https://repl.it/join/xgawjbbb-azanir
+
+dictionary = {}
+
+for i in range(3):
+  date_now = input('Введите дату:')
+  task_now = input('Введите задачу:')
+  dictionary[date_now] = task_now
+
+#test
+print(dictionary)
